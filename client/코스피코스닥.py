b2fc93ab93426e81b680b36fe23615fb063f7bdb
Sungpil Kim <enjoykunta111@gmail.com>
2023-11-27 01:53:32 +0900

    231126_종목코드완료, 일별데이터 진행중

--- client/코스피코스닥.py
+++ client/코스피코스닥.py
@@ -4,37 +4,21 @@
 가져온 종목들 중에서 그러한 종목들을 제외되도록 직접 필터링을 직접 해줘야한다.
 '''
 
+'''
+일봉들 요청에서는 재귀함수를 사용한다.
+재귀함수는 구조만 알면 아주아주 간단한 용어이다.
+
+같은 내용을 조건에 따라 반복하게 만드는 것이라고 보면된다.
+일봉 요청은 한번에 500일치를 가져올 수 있다. 그 이상 가져오려면 또 요청해야 하는데, 이때 재귀함수를 사용하면 된다.
+'''
+
 from requests import post
+import asyncio
 from icecream import ic
 import datetime
 import json
 import csv
 import os
-
-
-# class 메인:
-#     def __init__(self):
-        
-#         accessToken = self.로그인()
-#         ic("접근키 확인", accessToken)
-
-#         종목들 = TR요청들().코스피코스닥(accessCode=accessToken)
-#         ic(종목들)
-
-#     def 로그인(self) -> str:
-#         url = "https://openapi.ebestsec.co.kr:8080/oauth2/token"
-#         headers = {"Content-type": "application/x-www-form-urlencoded"}
-#         body = {
-#             "grant_type": "client_credentials",
-#             "appkey": " PSZmG6ZONgejhZUpa8FGkb9hBPi107y1FMZg",
-#             "appsecretkey": "2L3NKwyRDFi1DmOkCgp6jaMeShlDtO3z",
-#             "scope": "oob",
-#         }
-
-#         response = post(url, headers=headers, data=body)
-
-#         return response.json()["access_token"]
-
 
 class PriceRequestHandler:
     def stock_code_request(self, accessCode):
@@ -70,6 +54,7 @@ class PriceRequestHandler:
         # filler: filler
         return combined_results
         
+    # 종목코드 csv로 export
     def save_to_csv(self, data, filename):
         # data폴더 존재여부 확인
         if not os.path.exists('..\\data'):
@@ -82,6 +67,57 @@ class PriceRequestHandler:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(data)
+
+
+    # csv속 종목코드들의 일자별 Price data db에 저장하기 (csv로 뽑기부터 해보기)
+    #아래 메소드는 어디서 불러오는게 맞을까??
+    async def stock_price_request(self, accessCode,gubun,shcode,sdate, edate, tr_cont='N', tr_cont_key='', cts_date='' ):
+        url = "https://openapi.ebestsec.co.kr:8080/stock/chart"
+        headers = {
+            "Content-type": "application/json; charset=utf-8",
+            "authorization": f"Bearer {accessCode}",
+            "tr_cd": "t8410",
+            "tr_cont": tr_cont,
+            "tr_cont_key": tr_cont_key,
+        }
+        body = {
+            "t8410InBlock": {
+                "shcode": shcode,
+                "gubun": gubun,
+                "qrycnt": 500,
+                "sdate": sdate,
+                "edate": cts_date or edate,
+                # cts_date로만 조회가 먹히질 않음. 그러므로 edate를 바꿔가며 조회가 필요함, 결국 연속조회에서 cts_date를 넣지 않아도 됨.
+                # 또한 해당 날짜일 포함하여 검색된다.
+                "cts_date": "",
+                "comp_yn": "N",
+                "sujung": "Y",
+            }
+        }
+        response = post(url, headers=headers, data=json.dumps(body))
+
+        headers = response.headers
+        rTrContKey = headers["tr_cont_key"]
+        rTrCont = headers["tr_cont"]
+        ic(headers)
+
+        ctsResult = response.json()["t8410OutBlock"]
+        rCtsDate = ctsResult["cts_date"]
+        ic(rCtsDate)
+        
+
+        response.json()["t8410OutBlock1"]
+
+        if rTrCont == "Y":
+            await asyncio.sleep(2)
+            await self.일봉들요청(
+                accessCode=accessCode,
+                tr_cont=tr_cont,
+                tr_cont_key=rTrContKey,
+                cts_date=rCtsDate,
+            )
+
+        print(response.json()["t8410OutBlock1"])
 
 
 def time_format():
